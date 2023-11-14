544c52ece789023a68042499679ffa1f53963b53
fernandocaputoromero <caputoengenharia@hotmail.com>
2023-11-14 10:40:50 -0300

    proprieties,Game, ORM

---
+++ src/main/java/com/devsuperior/entities/Game.java.java
@@ -0,0 +1,175 @@
+package com.devsuperior.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+
+
+public class Game.java {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String plataforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+        
+    }
+
+    public Game(long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl,
+            String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.plataforms = plataforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    
+    /**
+     * @return long return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return Integer return the year
+     */
+    public Integer getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    /**
+     * @return String return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * @return String return the plataforms
+     */
+    public String getPlataforms() {
+        return plataforms;
+    }
+
+    /**
+     * @param plataforms the plataforms to set
+     */
+    public void setPlataforms(String plataforms) {
+        this.plataforms = plataforms;
+    }
+
+    /**
+     * @return Double return the score
+     */
+    public Double getScore() {
+        return score;
+    }
+
+    /**
+     * @param score the score to set
+     */
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    /**
+     * @return String return the imgUrl
+     */
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    /**
+     * @param imgUrl the imgUrl to set
+     */
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    /**
+     * @return String return the shortDescription
+     */
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    /**
+     * @param shortDescription the shortDescription to set
+     */
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    /**
+     * @return String return the longDescription
+     */
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    /**
+     * @param longDescription the longDescription to set
+     */
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+}
+    
+    
+
+   
